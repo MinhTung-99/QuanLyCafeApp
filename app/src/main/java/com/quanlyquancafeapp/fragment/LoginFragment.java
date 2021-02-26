27de6a61f071ab492bdfe2fda4fe9a5a174478a8
AppNative <tung07011999@gmail.com>
2021-02-26 16:34:01 +0700

    up

--- app/src/main/java/com/quanlyquancafeapp/fragment/LoginFragment.java
+++ app/src/main/java/com/quanlyquancafeapp/fragment/LoginFragment.java
@@ -9,10 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.quanlyquancafeapp.R;
+
 public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
