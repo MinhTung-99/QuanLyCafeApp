854a71051c113b0cd47fc7190c39e6f11217acf0
MinhTung-99 <tung07011999@gmail.com>
2021-02-27 08:31:50 +0700

    up

--- app/src/main/java/com/quanlyquancafeapp/fragment/LoginFragment.java
+++ app/src/main/java/com/quanlyquancafeapp/fragment/LoginFragment.java
@@ -17,7 +17,6 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
