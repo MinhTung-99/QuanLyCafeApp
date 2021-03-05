7ad5879550f0fb632067f945f98f82e1caca5842
futeam <fu89.info@gmail.com>
2021-03-05 21:31:48 +0700

    up

--- app/src/main/java/com/quanlyquancafeapp/fragment/PayFragment.java
+++ app/src/main/java/com/quanlyquancafeapp/fragment/PayFragment.java
@@ -1,6 +1,8 @@
 package com.quanlyquancafeapp.fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,20 +10,58 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.quanlyquancafeapp.R;
+import com.quanlyquancafeapp.databinding.FragmentPayBinding;
+import com.quanlyquancafeapp.utils.DataFake;
+import com.quanlyquancafeapp.utils.PriceUtil;
 
 public class PayFragment extends Fragment {
+    private FragmentPayBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pay, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pay, container, false);
+        return binding.getRoot();
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        float totalMoney = getArguments().getFloat("totalMoney");
-        Log.d("KMFG", totalMoney+" ====");
         super.onViewCreated(view, savedInstanceState);
+
+        float totalMoney = getArguments().getFloat("totalMoney");
+        String setupMoney = PriceUtil.setupPrice(String.valueOf(totalMoney));
+        binding.txtTotalMoney.setText(setupMoney);
+
+        binding.edtMoney.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().equals("")){
+                    binding.txtRefunds.setText(String.valueOf(0));
+                }else {
+                    float money = Float.parseFloat(s.toString());
+                    float moneyRefunds = money - totalMoney;
+                    String setupMoney = PriceUtil.setupPrice(String.valueOf(moneyRefunds));
+                    binding.txtRefunds.setText(setupMoney);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.btnPay.setOnClickListener(v->{
+            for(int i = 0; i < 2; i++){
+                Navigation.findNavController(v).popBackStack();
+                //DELETE TABLE ORDER
+            }
+        });
     }
 }
