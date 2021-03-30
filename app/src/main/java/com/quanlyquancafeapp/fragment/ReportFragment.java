5c938a3deb42c50e58e7294bb966bea5febe6135
futeam <fu89.info@gmail.com>
2021-03-30 23:55:12 +0700

    up

--- app/src/main/java/com/quanlyquancafeapp/fragment/ReportFragment.java
+++ app/src/main/java/com/quanlyquancafeapp/fragment/ReportFragment.java
@@ -1,12 +1,18 @@
 package com.quanlyquancafeapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -16,6 +22,8 @@ import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Pie;
+import com.quanlyquancafeapp.HomeActivity;
+import com.quanlyquancafeapp.HomeAdminActivity;
 import com.quanlyquancafeapp.R;
 import com.quanlyquancafeapp.databinding.FragmentReportBinding;
 
@@ -26,6 +34,14 @@ public class ReportFragment extends Fragment { ;
     private FragmentReportBinding binding;
     String[] drinks = {"Cafe sữa","Cafe đá","7 up", "Coca"};
     int[] earning = {500,800,2000,600};
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(binding.toolbar);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -43,8 +59,25 @@ public class ReportFragment extends Fragment { ;
         }
         pie.data(dataEntries);
         binding.anyChartView.setChart(pie);
-        binding.btnInvoice.setOnClickListener(v->{
-            Navigation.findNavController(getView()).navigate(R.id.invoiceFragment);
-        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_report, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_invoice:
+                Navigation.findNavController(getView()).navigate(R.id.invoiceFragment);
+                break;
+            case R.id.menu_revenue:
+                Toast.makeText(getContext(), "MENU",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
